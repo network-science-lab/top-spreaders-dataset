b37377b9bd027e7bdb8794b69850201548fa4fe1
Michał Czuba <michal.czuba.1995@gmail.com>
2024-07-30 13:26:17 +0200

    added final script for EDA

---
+++ eda_utils/csv_loader.py
@@ -0,0 +1,18 @@
+import pathlib
+import re
+
+import pandas as pd
+
+
+def get_simulation_params(file_name: pathlib.Path) -> tuple[str, str, str]:
+    pattern = r"proto-(OR|AND)--p-([\d.]+)--net-(.*)\.csv"
+    return re.match(pattern, file_name.name).groups()
+
+
+def read_csv(file_path: pathlib.Path) -> pd.DataFrame:
+    proto, p, net = get_simulation_params(file_path)
+    file_df = pd.read_csv(file_path)
+    file_df["network"] = net
+    file_df["protocol"] = proto
+    file_df["p"] = p
+    return file_df

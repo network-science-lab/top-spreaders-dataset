a09f81ad72137b5b8bc80cd18b47106a5da3c0ad
Michał Czuba <michal.czuba.1995@gmail.com>
2024-10-12 15:39:52 +0200

    removed RNG setting since it didn't work

--- nsl_data_utils/misc/generate.py
+++ nsl_data_utils/misc/generate.py
@@ -10,32 +10,17 @@ from nsl_data_utils.misc.generators import MultilayerERGenerator, MultilayerPAGe
 from tqdm.contrib.concurrent import process_map
 
 
-def generate_and_save(
-    model_type: str,
-    actors: int,
-    layers: int,
-    steps: int,
-    outfile: str,
-    random_state: int | None = None,
-) -> None:
+def generate_and_save(model_type: str, actors: int, layers: int, steps: int, outfile: str) -> None:
     """Generate network and save it as an mpx file."""
     if model_type == "PA":
         nb_hubs = int(max(3, 0.05 * actors))
         generator = MultilayerPAGenerator(
-            nb_layers=layers,
-            nb_actors=actors,
-            nb_steps=steps,
-            nb_hubs=nb_hubs,
-            random_state=random_state,
+            nb_layers=layers, nb_actors=actors, nb_steps=steps, nb_hubs=nb_hubs
         )
     elif model_type == "ER":
         std_nodes = int(0.1 * actors)
         generator = MultilayerERGenerator(
-            nb_layers=layers,
-            nb_actors=actors,
-            nb_steps=steps,
-            std_nodes=std_nodes,
-            random_state=random_state,
+            nb_layers=layers, nb_actors=actors, nb_steps=steps, std_nodes=std_nodes
         )
     else:
         raise ValueError("Model type can be either PA or ER!")
@@ -55,7 +40,6 @@ def parse_args(args=None) -> Namespace:
     parser.add_argument("min_layers", type=int)
     parser.add_argument("max_layers", type=int)
     parser.add_argument("model_type", type=str, choices=["ER", "PA"])
-    parser.add_argument("--random-state", type=int, default=None)
     parser.add_argument("-o", "--output-dir", type=str, default="./generated_graphs/")
     parser.add_argument("--n-jobs", type=int, default=1)
     parser.add_argument("--chunksize", type=int, default=1)
@@ -64,20 +48,12 @@ def parse_args(args=None) -> Namespace:
 
 def main(args: Namespace) -> None:
     """Main generation routine feed with CLI args."""
-
-    if args.random_state is not None:
-        np.random.seed(args.random_state)  # TODO!!!
-
     actors = np.random.randint(args.min_actors, args.max_actors + 1, dtype=np.int32, size=args.nb_networks)
     layers = np.random.randint(args.min_layers, args.max_layers + 1, dtype=np.int32, size=args.nb_networks)
     steps = actors * 5
     output_dir = Path(args.output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
     outfiles = (output_dir / f"network_{idx}.mpx" for idx, _ in enumerate(actors, 1))
-    random_states = (
-        np.random.random(size=len(actors)) * 10 ** (np.log10(len(actors)) + 1)
-    ).astype(np.int32)
-
     process_map(
         generate_and_save,
         [args.model_type] * len(actors),
@@ -85,7 +61,6 @@ def main(args: Namespace) -> None:
         layers,
         steps,
         outfiles,
-        random_states,
         max_workers=args.n_jobs,
         chunksize=args.chunksize,
     )
@@ -101,9 +76,8 @@ def cli():
             "5",
             "ER",
             "--output-dir", "/Users/michal/Development/infmax-trainer-icm-mln/krakrarkarka",
-            "--n-jobs", "10",
-            "--chunksize", "1",
-            "--random-state", "1",
+            "--n-jobs", "5",
+            "--chunksize", "20"
         ]
     )
     print(args)

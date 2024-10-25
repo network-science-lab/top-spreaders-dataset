abe43d2c196cc72b78c44dbabfff186b5c47b35e
Michał Czuba <michal.czuba.1995@gmail.com>
2024-10-25 08:43:11 +0000

    updated loader of raw SPs

--- nsl_data_utils/loaders/sp_loader.py
+++ nsl_data_utils/loaders/sp_loader.py
@@ -7,20 +7,7 @@ from typing import Any
 import pandas as pd
 from nsl_data_utils.loaders.constants import (
     ACTOR,
-    ARXIV_NETSCIENCE_COAUTHORSHIP,
-    ARXIV_NETSCIENCE_COAUTHORSHIP_MATH,
-    AUCS,
-    CANNES,
-    CKM_PHYSICIANS,
-    EU_TRANSPORTATION,
-    EU_TRANSPORT_KLM,
-    ER1,
-    ER2,
-    ER3,
-    ER5,
     EXPOSED,
-    FMRI74,
-    LAZEGA,
     MLN_SP_DATA_PATH,
     NETWORK,
     NOT_EXPOSED,
@@ -29,10 +16,21 @@ from nsl_data_utils.loaders.constants import (
     PEAK_ITERATION,
     PROTOCOL,
     SIMULATION_LENGTH,
-    SF1,
-    SF2,
-    SF3,
-    SF5,
+    ARTIFICIAL_ER,
+    ARTIFICIAL_PA,
+    ARTIFICIAL_SMALL,
+    ARXIV_NETSCIENCE_COAUTHORSHIP,
+    ARXIV_NETSCIENCE_COAUTHORSHIP_MATH,
+    AUCS,
+    CANNES,
+    CKM_PHYSICIANS,
+    EU_TRANSPORTATION,
+    EU_TRANSPORT_KLM,
+    FMRI74,
+    L2_COURSE_NET_1,
+    L2_COURSE_NET_2,
+    L2_COURSE_NET_3,
+    LAZEGA,
     TIMIK1Q2009,
     TOY_NETWORK,
 )
@@ -65,58 +63,76 @@ def _get_sp(csv_paths: list[str]) -> pd.DataFrame:
     return sp
 
 
-def _get_csv_paths(csv_regex: str) -> list[str]:
+def _get_csv_paths(csv_regex: str) -> list[Path]:
     return list(Path(MLN_SP_DATA_PATH).glob(csv_regex))
+
+
+def _sort_csv_paths(csv_regex: str):
+    """Reads all CSVs from a directory and sort it by th network's name."""
+    all_paths = _get_csv_paths(csv_regex=csv_regex)
+    sorted_paths = {}
+    for csv_path in all_paths:
+        net_name = csv_path.stem.split("-")[-1]
+        if not sorted_paths.get(net_name):
+            print(net_name, csv_path)
+            sorted_paths[net_name] = [csv_path]
+        else:
+            sorted_paths[net_name].append(csv_path)
+    return sorted_paths
+
+
+def _get_sp_bulk(net_group_name: str) -> dict[str, pd.DataFrame]:
+    """Bulk loader of SP for given network group."""
+    sp_dict = {}
+    for net_name, csv_paths in _sort_csv_paths(f"{net_group_name}/*.csv").items():
+        sp_dict[net_name] = _get_sp(csv_paths)
+    return sp_dict
 
 
 def _sp_not_implemented():
     raise NotImplementedError(f"Spreading potentials have been not prepared yet!")
 
 
-def load_sp(net_name: str) -> pd.DataFrame:
+def load_sp(net_name: str) -> dict[str, pd.DataFrame]:
     """Load spreading potentials dataset for given network."""
-    if net_name == FMRI74:
+    if net_name == ARTIFICIAL_ER:
+        return _get_sp_bulk(ARTIFICIAL_ER)
+    elif net_name == ARTIFICIAL_PA:
+        return _get_sp_bulk(ARTIFICIAL_PA)
+    elif net_name == ARTIFICIAL_SMALL:
+        return _get_sp_bulk("small_artificial")
+    elif net_name == FMRI74:
         return _get_sp(_get_csv_paths(f"{MLN_SP_DATA_PATH}/fmri74/*.csv"))
     elif net_name == ARXIV_NETSCIENCE_COAUTHORSHIP:
         csv_paths = [
             f for f in Path(f"{MLN_SP_DATA_PATH}/arxiv_netscience_coauthorship").rglob('**/*.csv') 
             if 'math.oc' not in f.parts
         ]
-        return _get_sp(csv_paths)
+        return {net_name: _get_sp(csv_paths)}
     elif net_name == ARXIV_NETSCIENCE_COAUTHORSHIP_MATH:
-        return _get_sp(_get_csv_paths(f"arxiv_netscience_coauthorship/math.oc/*.csv"))
+        return {net_name: _get_sp(_get_csv_paths(f"arxiv_netscience_coauthorship/math.oc/*.csv"))}
     elif net_name == AUCS:
-        return _get_sp(_get_csv_paths(f"small_real/*--net-aucs.csv"))
+        return {net_name: _get_sp(_get_csv_paths(f"small_real/*--net-aucs.csv"))}
     elif net_name == CANNES:
         _sp_not_implemented()
     elif net_name == CKM_PHYSICIANS:
-        return _get_sp(_get_csv_paths("small_real/*--net-ckm_physicians.csv"))
+        return {net_name: _get_sp(_get_csv_paths("small_real/*--net-ckm_physicians.csv"))}
     elif net_name == EU_TRANSPORTATION:
-        return _get_sp(_get_csv_paths("small_real/*--net-eu_transportation.csv"))
+        return {net_name: _get_sp(_get_csv_paths("small_real/*--net-eu_transportation.csv"))}
     elif net_name == EU_TRANSPORT_KLM:
-        return _get_sp(_get_csv_paths("small_real/*--net-eu_transport_klm.csv"))
+        return {net_name: _get_sp(_get_csv_paths("small_real/*--net-eu_transport_klm.csv"))}
+    elif net_name == L2_COURSE_NET_1:
+        return {net_name: _get_sp(_get_csv_paths("l2_course_net/*--net-l2_course_net_1.csv"))}
+    elif net_name == L2_COURSE_NET_2:
+        return {net_name: _get_sp(_get_csv_paths("l2_course_net/*--net-l2_course_net_2.csv"))}
+    elif net_name == L2_COURSE_NET_3:
+        return {net_name: _get_sp(_get_csv_paths("l2_course_net/*--net-l2_course_net_3.csv"))}
     elif net_name == LAZEGA:
-        return _get_sp(_get_csv_paths("small_real/*--net-lazega.csv"))
-    elif net_name == ER1:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-er1.csv"))
-    elif net_name == ER2:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-er2.csv"))
-    elif net_name == ER3:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-er3.csv"))
-    elif net_name == ER5:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-er5.csv"))
-    elif net_name == SF1:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-sf1.csv"))
-    elif net_name == SF2:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-sf2.csv"))
-    elif net_name == SF3:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-sf3.csv"))
-    elif net_name == SF5:
-        return _get_sp(_get_csv_paths("small_artificial/*--net-sf5.csv"))
+        return {net_name: _get_sp(_get_csv_paths("small_real/*--net-lazega.csv"))}
     elif net_name == TIMIK1Q2009:
-        return _get_sp(_get_csv_paths("timik1q2009/**/*.csv"))
+        return {net_name: _get_sp(_get_csv_paths("timik1q2009/**/*.csv"))}
     elif net_name == TOY_NETWORK:
-        return _get_sp(_get_csv_paths("small_real/*--net-toy_network.csv"))
+        return {net_name: _get_sp(_get_csv_paths("small_real/*--net-toy_network.csv"))}
     raise AttributeError(f"Unknown network: {net_name}")
 
 
@@ -143,3 +159,9 @@ def get_gt_data(net_name: str, protocol: str, p: float | None, budget: int) -> l
         ascending=[False, True, True, False]
     )
     return sp_mean.iloc[:budget][ACTOR].tolist()
+
+
+
+if __name__ == "__main__":
+    a = load_sp(ARTIFICIAL_SMALL)
+    print(a)

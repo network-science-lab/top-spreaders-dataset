574068e4da2e790871aec039409546557ae39a97
Michał Czuba <michal.czuba.1995@gmail.com>
2025-01-21 06:57:33 +0000

    modified sps loader to read batch_2 as well

--- nsl_data_utils/loaders/sp_loader.py
+++ nsl_data_utils/loaders/sp_loader.py
@@ -62,52 +62,69 @@ def _get_csv_paths(csv_regex: str) -> list[Path]:
     return list(Path(MLN_SP_DATA_PATH).glob(csv_regex))
 
 
-def _sort_csv_paths(csv_regex: str) -> dict[str, list[Path]]:
-    """Reads all CSVs from a directory and sort it by th network's name."""
-    all_paths = _get_csv_paths(csv_regex=csv_regex)
-    sorted_paths = {}
-    for csv_path in all_paths:
-        net_name = csv_path.stem.split("-")[-1]
-        if not sorted_paths.get(net_name):
-            sorted_paths[net_name] = [csv_path]
-        else:
-            sorted_paths[net_name].append(csv_path)
-    return sorted_paths
-
-
 def _sp_not_implemented():
     raise NotImplementedError(f"Spreading potentials have been not prepared yet!")
 
 
-def load_sp_paths(net_type: str, net_name: str) -> list[Path]:
-    """Load spreading potentials dataset for given network."""
-    if net_type in {ARTIFICIAL_ER, ARTIFICIAL_PA, ARTIFICIAL_SMALL}:
-        return _get_csv_paths(f"{net_type}/*--net-{net_name}.csv")
-    elif net_type == FMRI74:
-        return _get_csv_paths(f"fmri74/*.csv")
+def _load_sp_paths_batch_1(net_type: str, net_name: str) -> list[Path]:
+    if net_type in {
+        ARTIFICIAL_ER,
+        ARTIFICIAL_PA,
+        ARTIFICIAL_SMALL,
+        FMRI74,
+        L2_COURSE,
+    }:
+        return _get_csv_paths(f"batch_1/{net_type}/*--net-{net_name}.csv")
+    elif net_type in {
+        AUCS,
+        CKM_PHYSICIANS,
+        EU_TRANSPORTATION,
+        LAZEGA,
+        TOY_NETWORK,
+    }:
+        return _get_csv_paths(f"batch_1/small_real/*--net-{net_name}.csv")
     elif net_type == ARXIV_NETSCIENCE_COAUTHORSHIP:
         if net_name == ARXIV_NETSCIENCE_COAUTHORSHIP:
             return [
-                f for f in Path(f"{MLN_SP_DATA_PATH}/arxiv_netscience_coauthorship").rglob("**/*.csv") 
-                if "math.oc" not in f.parts
+                file_path for file_path in Path(
+                    f"{MLN_SP_DATA_PATH}/batch_1/arxiv_netscience_coauthorship"
+                ).rglob("**/*.csv") 
+                if "math.oc" not in file_path.parts
             ]
         elif net_name == ARXIV_NETSCIENCE_COAUTHORSHIP_MATH:
-            return _get_csv_paths(f"arxiv_netscience_coauthorship/math.oc/*.csv")
+            return _get_csv_paths(f"batch_1/arxiv_netscience_coauthorship/math.oc/*.csv")
         raise AttributeError(f"Unknown network name: {net_name} for {net_type}")
-    elif net_type == AUCS:
-        return _get_csv_paths(f"small_real/*--net-aucs.csv")
     elif net_type == CANNES:
         _sp_not_implemented()
-    elif net_type == CKM_PHYSICIANS:
-        return _get_csv_paths("small_real/*--net-ckm_physicians.csv")
-    elif net_type == EU_TRANSPORTATION:
-        return _get_csv_paths(f"small_real/*--net-{net_name}.csv")
-    elif net_type == L2_COURSE:
-        return _get_csv_paths(f"l2_course_net/*--net-{net_name}.csv")
-    elif net_type == LAZEGA:
-        return _get_csv_paths("small_real/*--net-lazega.csv")
     elif net_type == TIMIK1Q2009:
-        return _get_csv_paths("timik1q2009/**/*.csv")
-    elif net_type == TOY_NETWORK:
-        return _get_csv_paths("small_real/*--net-toy_network.csv")
+        return _get_csv_paths("batch_1/timik1q2009/**/*.csv")
     raise AttributeError(f"Unknown network: {net_type}")
+
+
+def _load_sp_paths_batch_2(net_type: str, net_name: str) -> list[Path]:
+    if net_type in {
+        ARTIFICIAL_ER,
+        ARTIFICIAL_PA,
+        ARTIFICIAL_SMALL,
+        ARXIV_NETSCIENCE_COAUTHORSHIP,
+        AUCS,
+        CKM_PHYSICIANS,
+        EU_TRANSPORTATION,
+        FMRI74,
+        L2_COURSE,
+        LAZEGA,
+        TIMIK1Q2009,
+        TOY_NETWORK
+    }:
+        return _get_csv_paths(f"batch_2/{net_type}/*--net-{net_name}.csv")
+    elif net_type == CANNES:
+        _sp_not_implemented()
+    raise AttributeError(f"Unknown network: {net_type}")
+
+
+def load_sp_paths(net_type: str, net_name: str) -> list[Path]:
+    """Load spreading potentials dataset for given network."""
+    return [
+        *_load_sp_paths_batch_1(net_name=net_name, net_type=net_type),
+        *_load_sp_paths_batch_2(net_name=net_name, net_type=net_type),
+    ]
